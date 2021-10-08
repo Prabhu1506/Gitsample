87325c96e07b91a7e61964ba4beebc4147b318ca
Prabhu1506 <prabakaranmathi1996@gmail.com>
2021-10-07 23:37:42 -0700

    username functionallity

--- src/org/test/Test1.java
+++ src/org/test/Test1.java
@@ -1,5 +1,18 @@
 package org.test;
 
 public class Test1 {
-
+	public static void userName() {
+	
+	System.out.println("username");
+	
+	}	
+public static void main(String[] args) {
+userName();	
 }
+	
+	
+	
+	
+	
+}
+
