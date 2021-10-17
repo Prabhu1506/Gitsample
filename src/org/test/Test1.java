02f7027a18d71235cdfb315e59f8c1183fc59236
Prabhu1506 <prabakaranmathi1996@gmail.com>
2021-10-17 00:54:31 -0700

    User

--- src/org/test/Test1.java
+++ src/org/test/Test1.java
@@ -2,17 +2,16 @@ package org.test;
 
 public class Test1 {
 	public static void userName() {
-	
-	System.out.println("username");
-	
-	}	
-public static void main(String[] args) {
-userName();	
-}
-	
-	
-	
-	
-	
-}
 
+		System.out.println("username");
+
+	}
+
+	public static void main(String[] args) {
+		userName();
+	}
+	private void sysout() {
+		// TODO Auto-generated method stub
+
+	}
+}
